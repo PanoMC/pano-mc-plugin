79fdacf7d32e33fb82933dcabc5855287531d3e6
kahverengi <ahmet@duruer.dev>
2025-08-16 15:10:26 +0300

    fix: fix not properly checking is spigot

--- src/main/kotlin/com/panomc/plugins/pano/spigot/SpigotServerUtil.kt
+++ src/main/kotlin/com/panomc/plugins/pano/spigot/SpigotServerUtil.kt
@@ -10,9 +10,10 @@ object SpigotServerUtil {
             ServerType.PAPER
         } catch (_: ClassNotFoundException) {
             val name = org.bukkit.Bukkit.getName().lowercase()
+            val version = org.bukkit.Bukkit.getVersion().lowercase()
             when {
                 name.contains("paper", true) -> ServerType.PAPER
-                name.contains("spigot", true) -> ServerType.SPIGOT
+                name.contains("spigot", true) || version.contains("spigot", true) -> ServerType.SPIGOT
                 name.contains("bukkit", true) -> ServerType.BUKKIT
                 else -> ServerType.SPIGOT
             }
