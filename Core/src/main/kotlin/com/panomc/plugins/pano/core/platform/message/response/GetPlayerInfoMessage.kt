ddb48f0c1574967c7f663f32fdc434f3ffcc83ee
kahverengi <ahmet@duruer.dev>
2025-11-09 23:29:14 +0300

    fix: fixed pending email to email when getting player info message

--- Core/src/main/kotlin/com/panomc/plugins/pano/core/platform/message/response/GetPlayerInfoMessage.kt
+++ Core/src/main/kotlin/com/panomc/plugins/pano/core/platform/message/response/GetPlayerInfoMessage.kt
@@ -6,6 +6,6 @@ data class GetPlayerInfoMessage(
     val registered: Boolean,
     val banned: Boolean,
     val verified: Boolean,
-    val pendingEmail: String? = null,
+    val email: String? = null,
     val locale: String? = null,
 ) : PlatformMessageResponse
